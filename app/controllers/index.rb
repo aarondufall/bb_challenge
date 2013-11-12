2d1d4f50a259d2a6574fe0f04ee6baaa2aabbb3d
Aaron Dufall <aald212@gmail.com>
2013-11-11 18:36:39 -0800

    challenge code

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,27 +1,15 @@
 get '/' do
   puts "[LOG] Getting /"
   puts "[LOG] Params: #{params.inspect}"
+  @words = read_words
   erb :index
 end
 
-get '/cool_url' do
-  puts "[LOG] Getting /cool_url"
-  puts "[LOG] Params: #{params.inspect}"
-  @text = params[:user_input]
-  @check = params[:check]
-  erb :get_cool_url
-end
-
-post '/cool_url' do
-  puts "[LOG] Posting to /cool_url"
-  puts "[LOG] Params: #{params.inspect}"
-  @request = request   # => '/hello-world'
-  @textarea = params[:text_area]
-  @textfield = params[:text_field]
-  erb :post_cool_url#,:layout => false
-end
-
-get '/splat/*' do
-  @value = params[:splat]
-  erb :get_cool_url
+def read_words
+  words = []
+ f = File.open(File.join(APP_ROOT, "app/input.txt"), "r")
+ f.each_line do |line|
+   words << line
+ end
+ words
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,65 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+var wordlist;
+var sortedWordList;
+var anagrams = {};
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+function updateAnagrams(){
+	content = ''
+	for (var a in anagrams) {
+		content += '<p>'+ a + ':'+ anagrams[a] +'</p>'
+		$('div#anagrams').html(content)
+	}
+}
+
+function findAnagram(words){
+	for(i = 0; words.length > i; i++){
+		anagrams[wordlist[i]] = 0
+		for(w = 0; sortedWordList.length > w; w++ ){
+			if (words[i] == sortedWordList[w]){
+				anagrams[wordlist[i]] += 1
+				updateAnagrams()
+			}
+		}
+	}
+	console.log(anagrams)
+}
+
+
+function sortAlpha(wordArr){
+	wordArr.sort(function(a, b){
+    if(a < b) return -1;
+    if(a > b) return 1;
+    return 0;
+})
+	return wordArr
+}
+
+function sortWordList(words){
+	var canWordList = [];
+	for (var i = 0; words.length > i; i++){
+		var sorted = sortAlpha(words[i].split(""))
+		canWordList.push(sorted.join("").toLowerCase())
+	}
+	return canWordList;
+}
+
+function processWordList(words){
+	var wList = []
+	for(i = 0; words.length > i; i++){
+  	wList.push($(words[i]).text());
+  }
+  return wList;
+}
+
+
+function getWordList(){
+	return $('#left-col p')
+}
+
+
+
+$(document).ready(function() {
+  wordlist = processWordList(getWordList());
+  sortedWordList = sortWordList(wordlist);
+  findAnagram(sortedWordList)
 });
